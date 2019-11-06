e0cc2b29f6d34d15f905369247e379f170efd346
Sugiyama <Henrique.Sugiyama@dieboldnixdorf.com>
2019-11-06 09:30:06 -0300

    Added  database connector and test first register

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,15 @@
 import { Router } from "express";
 
+import User from './app/models/User';
+
 const routes = new Router();
 
-routes.get("/", (req, res) => {
+routes.get("/", async (req, res) => {
+  const user = await User.create({
+    name: 'henrique',
+    email: 'h.sugiyama@a.com',
+    password_hash: '123456',
+  })
   return res.json({ message: "Hi henry" });
 });
 
